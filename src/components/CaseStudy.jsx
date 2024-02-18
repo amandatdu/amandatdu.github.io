282e51d2a42edd34832e2f686487bc525a3ade44
amandatdu <amandatdu22@gmail.com>
2024-02-18 15:40:31 -0500

    Intuit case
    added all images
    added all copy
    made case study header
    working on case study buttons

--- src/components/CaseStudy.jsx
+++ src/components/CaseStudy.jsx
@@ -1,15 +1,62 @@
 import "./CaseStudy.css";
 
-export const CaseStudy = ({ title, description, image}) => {
+export const CaseStudy = ({
+    title,
+    children,
+    image,
+    isFullImage,
+    imageClass = ""
+  }) => {
     return (
       <div className="caseStudy">
-        <div className="caseImage">
+        <div className={`caseImage ${imageClass} ${isFullImage ? "full" : ""}`}>
           {image}
         </div>
         <div className="caseContent">
           <h1> {title} </h1>
-          <p> {description} </p>
+          <p> {children} </p>
         </div>
       </div>
+    );
+};
+
+export const CaseIntro= ({
+    title,
+    children,
+    collaborators,
+    team,
+  }) => {
+    return (
+      <div className="caseIntro">
+        <div >
+          <h2> {title} </h2>
+          <p> {children} </p>
+        </div>
+        <div className="caseContext">
+        <div>
+          <h3> Collaborators </h3>
+          <p> {collaborators} </p>
+        </div>
+        <div>
+          <h3> Team</h3>
+          <p> {team} </p>
+        </div>
+        </div>
+        </div>
+
+    );
+};
+
+
+export const SkipButton= ({
+    copy,
+    buttoncolour,
+  }) => {
+    return (
+      <div className="buttonStyle">
+      background-color: {buttoncolour};
+      <button onClick={() => console.log('Button clicked')}>{copy}}</button>
+    </div>
+
     );
 };
