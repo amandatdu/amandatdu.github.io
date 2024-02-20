56954fbae33ed64f1f261ee928ad40e03a5f6e63
Selina Li <selinali2010@gmail.com>
2024-02-20 00:17:24 -0500

    Fix margins and refactor case theming

--- src/components/CaseStudy.jsx
+++ src/components/CaseStudy.jsx
@@ -1,148 +1,160 @@
 import "./CaseStudy.css";
 
-export const CaseHeader = ({ title, caseName, logo, children, textColor }) => {
-  return (
-    <div className={"caseHeader " + caseName}>
-      <div
-        style={{
-          flexDirection: "column",
-          width: "434px",
-          textAlign: "center",
-          padding: "160px",
-          color: `${textColor}`
-        }}
-      >
-        <div>{logo ? logo : null}</div>
-        <h1> {title} </h1>
-        <p style={{color: `${textColor}`}}> {children} </p>
-      </div>
-    </div>
-  );
-};
-
-
-export const CaseHMW = ({HMWstatement, title, backgroundColor}) => {
-  return (
-    <div className="caseHMW" style={{backgroundColor: `${backgroundColor}`, alignItems: "center", justifyContent:"center", display:"flex"}}>
-      <div style={{
-        width:"80%",
-      }}>
-        <h3 style={{
-          paddingBottom: "24px",
-        }}> {title} </h3>
-        <h2> {HMWstatement} </h2>
+export const CaseHeader = ({ title, caseName, logo, children, theme }) => {
+    return (
+        <div className={"caseHeader " + caseName}>
+            <div
+                style={{
+                    flexDirection: "column",
+                    width: "434px",
+                    textAlign: "center",
+                    padding: "160px",
+                }}
+                className={`color--${theme}`}
+            >
+                <div>{logo ? logo : null}</div>
+                <h1> {title} </h1>
+                <p className={`color--${theme}`}> {children} </p>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
-
+export const CaseHMW = ({ HMWstatement, title, backgroundColor }) => {
+    return (
+        <div
+            className="caseHMW"
+            style={{
+                backgroundColor: `${backgroundColor}`,
+                alignItems: "center",
+                justifyContent: "center",
+                display: "flex",
+            }}
+        >
+            <div
+                style={{
+                    width: "80%",
+                }}
+            >
+                <h3
+                    style={{
+                        paddingBottom: "24px",
+                    }}
+                >
+                    {" "}
+                    {title}{" "}
+                </h3>
+                <h2> {HMWstatement} </h2>
+            </div>
+        </div>
+    );
+};
 
 export const CaseStudy = ({
-  title,
-  children,
-  image,
-  isFullImage,
-  imageClass,
-  idName,
-  textColor,
-  buttonBar
+    title,
+    children,
+    image,
+    isFullImage,
+    imageClass,
+    idName,
+    theme,
+    buttonBar,
+    hideBorder,
 }) => {
-  return (
-    <div className="caseStudy" id={idName ? `${idName}` : ""}>
-      <div
-        className={`caseImage ${imageClass ? imageClass : ""}${
-          isFullImage ? "full" : ""
-        }`}
-      >
-        {image}
-      </div>
-      <div className={"caseContent" + `${buttonBar ? " buttons" : ""}`}>
-        <h1 style={{color: `${textColor}`}}> {title} </h1>
-        <p> {children} </p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="caseStudy" id={idName ? `${idName}` : ""}>
+            <div
+                className={`caseImage ${imageClass ? imageClass : ""}${
+                    isFullImage ? "full" : ""
+                } ${hideBorder ? "hideBorder" : ""}`}
+            >
+                {image}
+            </div>
+            <div className={`caseContent ${buttonBar ? " buttons" : ""}`}>
+                <h1 className={`color--${theme}`}> {title} </h1>
+                <p> {children} </p>
+            </div>
+        </div>
+    );
 };
 
-export const CaseIntro = ({ title, children, collaborators, team, textColor }) => {
-  return (
-    <div className="caseIntro">
-      <div>
-        <h2 style={{color: `${textColor}`}}> {title} </h2>
-        <p> {children} </p>
-      </div>
-      <div className="caseContext">
-        <div>
-          <h3 style={{color: `${textColor}`}}> Collaborators </h3>
-          <p> {collaborators} </p>
+export const CaseIntro = ({ title, children, collaborators, team, theme }) => {
+    return (
+        <div className={`caseIntro color--${theme}`}>
+            <div>
+                <h2> {title} </h2>
+                <p> {children} </p>
+            </div>
+            <div className="caseContext">
+                <div>
+                    <h3> Collaborators </h3>
+                    <p> {collaborators} </p>
+                </div>
+                <div>
+                    <h3> Team</h3>
+                    <p> {team} </p>
+                </div>
+            </div>
         </div>
-        <div>
-          <h3 style={{color: `${textColor}`}}> Team</h3>
-          <p> {team} </p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export const SkipButton = ({ buttonText, targetId, buttonColour }) => {
-  const handleClick = () => {
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-  };
+    const handleClick = () => {
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }
+    };
 
-  return (
-    <div
-      onClick={handleClick}
-      style={{
-        backgroundColor: `${buttonColour}`,
-        borderRadius: "30px",
-        padding: "15px",
-        paddingRight: "30px",
-        paddingLeft: "30px",
-        textAlign: "center",
-        justifyContent: "center",
-        alignItems: "center",
-        whiteSpace: "nowrap",
-        margin: "2%",
-        color:"white"
-      }}
-    >
-      <div>
-      {buttonText}
-      </div>
-    </div>
-  );
+    return (
+        <div
+            onClick={handleClick}
+            style={{
+                backgroundColor: `${buttonColour}`,
+                borderRadius: "30px",
+                padding: "15px",
+                paddingRight: "30px",
+                paddingLeft: "30px",
+                textAlign: "center",
+                justifyContent: "center",
+                alignItems: "center",
+                whiteSpace: "nowrap",
+                margin: "2%",
+                color: "white",
+            }}
+        >
+            <div>{buttonText}</div>
+        </div>
+    );
 };
 
 export const LinkButton = ({ buttonText, targetUrl, buttonColour }) => {
-  const handleClick = () => {
-    window.open(targetUrl, '_blank');
-  };
+    const handleClick = () => {
+        window.open(targetUrl, "_blank");
+    };
 
-  return (
-    <div
-      onClick={handleClick}
-      style={{
-        backgroundColor: `${buttonColour}`,
-        borderRadius: "30px",
-        padding: "15px",
-        paddingRight: "30px",
-        paddingLeft: "30px",
-        textAlign: "center",
-        justifyContent: "center",
-        alignItems: "center",
-        whiteSpace: "nowrap",
-        margin: "2%",
-        color:"white"
-      }}
-    >
-      <div>
-      {buttonText}
-      </div>
-    </div>
-  );
+    return (
+        <div
+            onClick={handleClick}
+            style={{
+                backgroundColor: `${buttonColour}`,
+                borderRadius: "30px",
+                padding: "15px",
+                paddingRight: "30px",
+                paddingLeft: "30px",
+                textAlign: "center",
+                justifyContent: "center",
+                alignItems: "center",
+                whiteSpace: "nowrap",
+                margin: "2%",
+                color: "white",
+            }}
+        >
+            <div>{buttonText}</div>
+        </div>
+    );
 };
--- src/pages/LCBOPage.jsx
+++ src/pages/LCBOPage.jsx
@@ -1,87 +1,98 @@
 import ProgressBar from "react-scroll-progress-bar";
-import { CaseStudy, CaseHeader, CaseIntro, LinkButton } from "../components/CaseStudy";
+import {
+    CaseStudy,
+    CaseHeader,
+    CaseIntro,
+    LinkButton,
+} from "../components/CaseStudy";
 import "./LCBOPage.css";
 
 export const LCBOPage = () => {
-    const textColor = "#173F35"
-  return (
-    <>
-    <ProgressBar bgcolor="#FF6EA2"/>
-    <div className="lcboPage">
-    <CaseHeader
-          caseName="lcbo"
-          title="LCBOdesk –
+    const pageName = "lcbo";
+    const themeColor = "var(--lcbo-green)";
+
+    return (
+        <>
+            <ProgressBar bgcolor="#FF6EA2" />
+            <div className="lcboPage" style={{ "--case-theme": themeColor }}>
+                <CaseHeader
+                    caseName="lcbo"
+                    title="LCBOdesk –
           streamlining user feedback"
-          children="UX/UI Co-op ☆ May 2021 – Aug 2021"
-          textColor={textColor}
-        />
-      <CaseIntro
-        title="As the innovation team, its our job to help LCBO evolve."
-        collaborators={
-          <>
-            1 Product Manager
-            <br />
-            1 Product Designer
-            <br />
-            2 Developers
-            <br />
-          </>
-        }
-        team="LCBOnext Innovation"
-        textColor={textColor}
-      >
-        <h3>
-          This was my first UX/UI internship ever! All I wanted to do was find problems I could help solve.
-        </h3>
-        At the beginning of our term, the team noticed an issue: we rarely seem to receive our customer’s complaints or issues.
-        <br /> <br />
-        We were so eager to grow and learn, so we wanted to take matters into our own hands and sought out different ways to help LCBO. We noticed that a lot of our customers who have issues never actually reach us. Everything keeps getting lost in translation.
-        <br /> <br />
-        Due to NDA purposes, my case study can be found on the LCBOnext website.
-      </CaseIntro>
+                    children="UX/UI Co-op ☆ May 2021 – Aug 2021"
+                    theme={pageName}
+                />
+                <CaseIntro
+                    title="As the innovation team, its our job to help LCBO evolve."
+                    collaborators={
+                        <>
+                            1 Product Manager
+                            <br />
+                            1 Product Designer
+                            <br />
+                            2 Developers
+                            <br />
+                        </>
+                    }
+                    team="LCBOnext Innovation"
+                    theme={pageName}
+                >
+                    <h3>
+                        This was my first UX/UI internship ever! All I wanted to
+                        do was find problems I could help solve.
+                    </h3>
+                    At the beginning of our term, the team noticed an issue: we
+                    rarely seem to receive our customer’s complaints or issues.
+                    <br /> <br />
+                    We were so eager to grow and learn, so we wanted to take
+                    matters into our own hands and sought out different ways to
+                    help LCBO. We noticed that a lot of our customers who have
+                    issues never actually reach us. Everything keeps getting
+                    lost in translation.
+                    <br /> <br />
+                    Due to NDA purposes, my case study can be found on the
+                    LCBOnext website.
+                </CaseIntro>
 
-      <CaseStudy
-       buttonBar={true}>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "100%",
-              marginTop: "-20%"
-            }}
-          >
-          <LinkButton
-            targetUrl="https://www.lcbonext.com/copy-of-inline-spotlight"
-            buttonText="Read my case study"
-            buttonColour={textColor}
-          />
+                <CaseStudy theme={pageName} buttonBar={true}>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "100%",
+                            marginTop: "-20%",
+                        }}
+                    >
+                        <LinkButton
+                            targetUrl="https://www.lcbonext.com/copy-of-inline-spotlight"
+                            buttonText="Read my case study"
+                            buttonColour={themeColor}
+                        />
 
-        <br />
-          <br />
-          <br />
-          <img
-            src="lcbo/logo.png"
-            alt="<LCBO desk logo>"
-            style={{width:"60%"}}
-          />
-          <br />
-          <br />
-          <br />
-          </div>
-          <div style={{height: "1px", width: "811px", backgroundColor: `${textColor}`, gap:"90px"}} />
-          </CaseStudy>
-
-
-
-
-
-
-
-
-
-    </div>
-    </>
-  );
+                        <br />
+                        <br />
+                        <br />
+                        <img
+                            src="lcbo/logo.png"
+                            alt="<LCBO desk logo>"
+                            style={{ width: "60%" }}
+                        />
+                        <br />
+                        <br />
+                        <br />
+                    </div>
+                    <div
+                        style={{
+                            height: "1px",
+                            width: "811px",
+                            backgroundColor: themeColor,
+                            gap: "90px",
+                        }}
+                    />
+                </CaseStudy>
+            </div>
+        </>
+    );
 };
