e911830c1ede499fc192707c4563333b3d1d716d
Selina Li <selinali2010@gmail.com>
2024-02-21 21:54:35 -0500

    Update banner stars and rotate

--- src/components/StarBanner.jsx
+++ src/components/StarBanner.jsx
@@ -4,28 +4,29 @@ export const StarBanner = () => {
     return (
         <div className="starBanner">
             {/* Left side */}
-            <SparkleIcon top="0%" left="2%" size="32px" />
-            <SparkleIcon top="68%" left="4%" size="32px" />
-            <SparkleIcon top="92%" left="10%" size="16px" />
-            <SparkleIcon top="82%" left="25%" size="16px" />
-            <SparkleIcon top="49%" left="14%" size="4px" />
-            <SparkleIcon top="22%" left="12%" size="8px" />
-            <SparkleIcon top="4%" left="21%" size="4px" />
-            <SparkleIcon top="34%" left="22%" size="12px" />
-            <SparkleIcon top="71%" left="19%" size="8px" />
-            <SparkleIcon top="9%" left="31%" size="16px" />
+            <SparkleIcon top="0%" left="2%" size="32px" angle={5} />
+            <SparkleIcon top="68%" left="4%" size="32px" angle={50} />
+            <SparkleIcon top="92%" left="10%" size="16px" angle={76} />
+            <SparkleIcon top="82%" left="25%" size="16px" angle={4} />
+            <SparkleIcon top="49%" left="16%" size="36px" angle={89} />
+            <SparkleIcon top="22%" left="12%" size="16px" angle={49} />
+            <SparkleIcon top="4%" left="21%" size="8px" angle={37} />
+            <SparkleIcon top="34%" left="22%" size="12px" angle={12} />
+            <SparkleIcon top="71%" left="19%" size="8px" angle={2} />
+            <SparkleIcon top="9%" left="31%" size="24px" angle={90} />
+            <SparkleIcon top="45%" left="7%" size="16px" angle={90} />
 
             {/* Right side */}
-            <SparkleIcon top="2%" left="75%" size="4px" />
-            <SparkleIcon top="3%" left="83%" size="8px" />
-            <SparkleIcon top="26%" left="76%" size="16px" />
-            <SparkleIcon top="17%" left="88%" size="32px" />
-            <SparkleIcon top="12%" left="96%" size="8px" />
-            <SparkleIcon top="36%" left="90%" size="16px" />
-            <SparkleIcon top="58%" left="82%" size="32px" />
-            <SparkleIcon top="65%" left="93%" size="8px" />
-            <SparkleIcon top="89%" left="76%" size="8px" />
-            <SparkleIcon top="79%" left="87%" size="16px" />
+            <SparkleIcon top="2%" left="75%" size="8px" angle={65} />
+            <SparkleIcon top="3%" left="83%" size="8px" angle={80} />
+            <SparkleIcon top="26%" left="76%" size="16px" angle={75} />
+            <SparkleIcon top="17%" left="88%" size="32px" angle={33} />
+            <SparkleIcon top="12%" left="96%" size="8px" angle={14} />
+            <SparkleIcon top="36%" left="90%" size="16px" angle={22} />
+            <SparkleIcon top="58%" left="82%" size="32px" angle={135} />
+            <SparkleIcon top="65%" left="93%" size="16px" angle={73} />
+            <SparkleIcon top="89%" left="76%" size="8px" angle={57} />
+            <SparkleIcon top="79%" left="87%" size="16px" angle={20} />
         </div>
     );
 };
